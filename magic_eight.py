a34ea340c96c886aa076d9b83906c7f79c854c62
June Park <juneplusalpha@icloud.com>
2018-01-29 13:51:26 -0500

    pick random answer

--- magic_eight.py
+++ magic_eight.py
@@ -3,5 +3,8 @@ def ask_question():
     question = input("What's your question?")
     return question
 
+import random
+def pick_answer():
+	answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
-
+	return random.choice(answers)
